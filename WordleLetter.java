a2d043c7471cd3578bb1f0db52bc3a6a9decdc22
ElianPeRa <168603731+ElianPeRa@users.noreply.github.com>
2025-10-22 22:20:25 -0600

    Clean up comments in WordleLetter.java
    Removed author and course information comments from the file.

--- WordleLetter.java
+++ WordleLetter.java
@@ -1,21 +1,6 @@
 //Author:Elian Pena Ramos
-//CS 1101
-//TA:Alireza Nouri
 //4/22/24
-//Instructor:Daniel Mejia
-//CL 3
 //Wordle
-/*
-[CS1101] Comprehensive Lab 3
-This work is to be done individually. It is not permitted to.
-share, reproduce, or alter any part of this assignment for any
-purpose. Students are not permitted to share code, upload
-this assignment online in any form, or view/receive/
-modifying code written by anyone else. This assignment is part.
-of an academic course at The University of Texas at El Paso and
-a grade will be assigned for the work produced individually by
-the student.
-*/
 public class WordleLetter {
 	private char letter;
 	private String color;
